c44bb2f1d1185083d8a60c47a1072b59f3d04731
Joonsup Lee <flamewizkey@naver.com>
2023-08-24 20:40:12 +0900

    second commit

--- js/main.js
+++ js/main.js
@@ -1,76 +1,79 @@
 // rec slider
 
-$('.product').slick({
-  dots: true,  
-  infinite: true,
-  speed: 300,  
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  autoplay:true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+$(document).ready(function(){
+  $('.product').slick({
+    dots: true,  
+    infinite: true,
+    speed: 300,  
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay:true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
       }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+    ]
+  });
+  
+  // new slider
+  
+  $('.new_product').slick({
+    dots: true,  
+    infinite: true,
+    speed: 300,  
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay:true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
       }
-    }
-  ]
-});
+    ]
+  });
+  
+  // shop slider
+  
+  $('.shop_product').slick({  
+    infinite: false,  
+    slidesToShow: 3,  
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2.5,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+})
 
-// new slider
-
-$('.new_product').slick({
-  dots: true,  
-  infinite: true,
-  speed: 300,  
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  autoplay:true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    }
-  ]
-});
-
-// shop slider
-
-$('.shop_product').slick({  
-  infinite: false,  
-  slidesToShow: 3,  
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2.5,
-        slidesToScroll: 2
-      }
-    }
-  ]
-});
 
 // scroll event
 
